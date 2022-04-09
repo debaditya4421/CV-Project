327c9fcf69f56fd0ccce83086180017082097a26
JebronLames32 <95158195+JebronLames32@users.noreply.github.com>
2022-04-09 20:54:19 +0530

    Task1.3 BFS

---
+++ BFStask1.3.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('homemap.jpeg', 0)
+n,m=img.shape
+for i in range (n):
+    for j in range(m):
+        if img[i,j]<243:
+            img[i,j]=0
+        else:
+            img[i,j]=255
+
+
+
+kernel = np.ones((2,2), np.uint8)
+ 
+
+img = cv2.dilate(img, kernel, iterations=1)
+
+a,b=(8,23)
+c,d=(51,26)
+img[a,b]=127
+img[c,d]=127
+cv2.namedWindow('dilated', cv2.WINDOW_NORMAL)
+cv2.imshow('dilated', img.astype(np.uint8))
+img[a][b] = 255
+img[c][d] = 255
+img1 = np.full((n, m), 0)
+#img2 = np.full((n, m), 255)
+img1[a][b] = 1
+add = 5
+d1 = 25
+
+
+def step(k):
+    global d1
+    d1 = 25
+    for i in range(n):
+        for j in range(m):
+            if img1[i][j] == k:
+                # global d1
+                d1 = 20
+                if (i > 0) and (img1[i - 1][j] == 0) and (img[i - 1][j] == 255):
+                    img1[i - 1][j] = k + 1
+                if (j > 0) and (img1[i][j - 1] == 0) and (img[i][j - 1] == 255):
+                    img1[i][j - 1] = k + 1
+                if (i + 1 < n) and (img1[i + 1][j] == 0) and (img[i + 1][j] == 255):
+                    img1[i + 1][j] = k + 1
+                if (j + 1 < m) and (img1[i][j + 1] == 0) and (img[i][j + 1] == 255):
+                    img1[i][j + 1] = k + 1
+    if d1 == 25:
+        global add
+        add = 10
+k = 0
+while add == 5:
+    k = k + 1
+    step(k)
+
+print(k)
+print(img1[a][b])
+cv2.namedWindow('NewImage', cv2.WINDOW_NORMAL)
+
+p = img1[c][d]
+#img2[c][d] = 0
+def path1(r,s,p):
+    global c
+    global d
+    if (r > 0) and (img1[r - 1][s] == p - 1):
+        img[r - 1][s] = 127
+        c = r-1
+        d = s
+        cv2.imshow('NewImage', img)
+        cv2.waitKey(100)
+
+    elif (s > 0) and (img1[r][s - 1] == p - 1):
+        img[r][s - 1] = 127
+        c = r
+        d = s - 1
+        cv2.imshow('NewImage', img)
+        cv2.waitKey(100)
+    elif (r + 1 < n) and (img1[r + 1][s] == p - 1):
+        img[r + 1][s] = 127
+        c = r + 1
+        d = s
+        cv2.imshow('NewImage', img)
+        cv2.waitKey(100)
+    elif (s + 1 < m) and (img1[r][s + 1] == p - 1):
+        img[r][s + 1] = 127
+        c = r
+        d = s+1
+        cv2.imshow('NewImage', img)
+        cv2.waitKey(100)
+while p>1:
+    path1(c,d,p)
+    p = p-1
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
